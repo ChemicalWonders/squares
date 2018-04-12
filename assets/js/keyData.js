aafb78518a50fddebe3cb16087fc9b16e1d2f6ae
Kevin Chan <kchan039@gmail.com>
2018-04-11 18:02:48 -0700

    inital commit, added square files, and sounds

---
+++ assets/js/keyData.js
@@ -0,0 +1,159 @@
+var data = {
+	a: {
+		color: "purple",
+		sound: new Howl({
+			src:['./assets/sounds/bubbles.mp3']
+		})
+	},
+	s: {
+		color: "red",
+		sound: new Howl({
+			src:['./assets/sounds/clay.mp3']
+		})
+	},
+	d: {
+		color: "lightpink",
+		sound: new Howl({
+			src:['./assets/sounds/confetti.mp3']
+		})
+	},
+	f: {
+		color: "lightgreen",
+		sound: new Howl({
+			src:['./assets/sounds/corona.mp3']
+		})
+	},
+	g: {
+		color: "lightgoldenrodyellow",
+		sound: new Howl({
+			src:['./assets/sounds/dotted-spiral.mp3']
+		})
+	},
+	h: {
+		color: "lightcyan",
+		sound: new Howl({
+			src:['./assets/sounds/flash-1.mp3']
+		})
+	},
+	j: {
+		color: "violet",
+		sound: new Howl({
+			src:['./assets/sounds/flash-2.mp3']
+		})
+	},
+	k: {
+		color: "orange",
+		sound: new Howl({
+			src:['./assets/sounds/flash-3.mp3']
+		})
+	},
+	l: {
+		color: "magenta",
+		sound: new Howl({
+			src:['./assets/sounds/glimmer.mp3']
+		})
+	},
+	q: {
+		color: "skyblue",
+		sound: new Howl({
+			src:['./assets/sounds/moon.mp3']
+		})
+	},
+	w: {
+		color: "brown",
+		sound: new Howl({
+			src:['./assets/sounds/pinwheel.mp3']
+		})
+	},
+	e: {
+		color: "springgreen",
+		sound: new Howl({
+			src:['./assets/sounds/piston-1.mp3']
+		})
+	},
+	r: {
+		color: "steelblue",
+		sound: new Howl({
+			src:['./assets/sounds/piston-2.mp3']
+		})
+	},
+	t: {
+		color: "teal",
+		sound: new Howl({
+			src:['./assets/sounds/piston-3.mp3']
+		})
+	},
+	y: {
+		color: "tan",
+		sound: new Howl({
+			src:['./assets/sounds/prism-1.mp3']
+		})
+	},
+	u: {
+		color: "turquoise",
+		sound: new Howl({
+			src:['./assets/sounds/prism-3.mp3']
+		})
+	},
+	i: {
+		color: "sienna",
+		sound: new Howl({
+			src:['./assets/sounds/splits.mp3']
+		})
+	},
+	o: {
+		color: "seagreen",
+		sound: new Howl({
+			src:['./assets/sounds/squiggle.mp3']
+		})
+	},
+	p: {
+		color: "salmon",
+		sound: new Howl({
+			src:['./assets/sounds/strike.mp3']
+		})
+	},
+	z: {
+		color: "royalblue",
+		sound: new Howl({
+			src:['./assets/sounds/suspension.mp3']
+		})
+	},
+	x: {
+		color: "powderblue",
+		sound: new Howl({
+			src:['./assets/sounds/timer.mp3']
+		})
+	},
+	c: {
+		color: "peachpuff",
+		sound: new Howl({
+			src:['./assets/sounds/ufo.mp3']
+		})
+	},
+	v: {
+		color: "paleturquoise",
+		sound: new Howl({
+			src:['./assets/sounds/veil.mp3']
+		})
+	},
+	b: {
+		color: "moccasin",
+		sound: new Howl({
+			src:['./assets/sounds/wipe.mp3']
+		})
+	},
+	n: {
+		color: "mintcream",
+		sound: new Howl({
+			src:['./assets/sounds/zig-zag.mp3']
+		})
+	},
+	m: {
+		color: "lightskyblue",
+		sound: new Howl({
+			src:['./assets/sounds/prism-2.mp3']
+		})
+	}
+
+}
